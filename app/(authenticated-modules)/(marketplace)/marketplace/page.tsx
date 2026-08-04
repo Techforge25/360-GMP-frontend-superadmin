5ba8b0564f29ada5d36c0d810cbe7d7e7ede6722
Techforge25 <github@techforgeinnovations.com>
2026-08-04 14:41:29 +0500

    addedd main page in marketplace

--- app/(authenticated-modules)/(marketplace)/marketplace/page.tsx
+++ app/(authenticated-modules)/(marketplace)/marketplace/page.tsx
@@ -1,29 +1,8 @@
-import React from "react";
-import OverviewCards from "@/components/common/OverviewCards";
-import { MarketPlaceCard } from "@/constants/marketplace/MarketPlaceCard";
-import MarketPlace from "@/components/marketplace/MarketPlace";
+import MarketPlaceMain from "@/components/marketplace/MarketPlaceMain";
+
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-background p-6 md:p-4 font-sans">
-      <OverviewCards
-        heading="Marketplace Operation Center"
-        description="Manage order logs product audit queue and disputed orders"
-        cards={MarketPlaceCard}
-        className="sm:grid-cols-4! lg:grid-cols-4!"
-        dropdown={
-          <select className="border border-border bg-white px-3 py-2 rounded-xl text-sm font-medium text-gray-700 outline-none">
-            <option>All Time</option>
-            <option>Last 7 Days</option>
-            <option>Last 1 Month</option>
-            <option>Last 6 Month</option>
-            <option>Last Year</option>
-          </select>
-        }
-      />
-      <div className="grid grid-cols-1  gap-[1.5rem] mt-6">
-        <MarketPlace />
-      </div>
-    </div>
+   <MarketPlaceMain/>
   );
 }
