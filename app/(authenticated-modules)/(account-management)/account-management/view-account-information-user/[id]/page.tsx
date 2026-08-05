05e9375e0f9f679e4e2b02f7d7db70fe4b61ac76
Techforge25 <github@techforgeinnovations.com>
2026-08-05 13:19:03 +0500

    fixed account management user preview ui fixed

--- app/(authenticated-modules)/(account-management)/account-management/view-account-information-user/[id]/page.tsx
+++ app/(authenticated-modules)/(account-management)/account-management/view-account-information-user/[id]/page.tsx
@@ -1,5 +1,4 @@
 import AccountManagementUser from '@/components/account-management/user-management/user-view-profile/AccountManagementUser'
-import React from 'react'
 
 export default function page() {
   return (
--- components/account-management/user-management/user-view-profile/AccountManagementProfile.tsx
+++ components/account-management/user-management/user-view-profile/AccountManagementProfile.tsx
@@ -3,8 +3,8 @@ import { FiMail } from "react-icons/fi";
 
 export default function ProfileCard() {
   return (
-    <div className="p-[1.5rem] border border-gray-200 rounded-[0.5rem] bg-white font-sans">
-      <div className="flex items-center gap-[1rem] p-[1rem] bg-bg-white-light rounded-[0.5rem] border border-gray-100">
+    <div className="p-[1rem] border border-gray-200 rounded-[0.75rem] bg-surface-DEFAULT font-sans">
+      <div className="flex items-center gap-[1rem] p-[1rem] bg-bg-gray-200 rounded-[0.5rem] border border-border-gray-200">
         <div className="relative w-[3.5rem] h-[3.5rem] rounded-[0.5rem] overflow-hidden border border-gray-200 bg-white">
           <img
             src="https://i.pravatar.cc/150?img=11" 
@@ -13,23 +13,23 @@ export default function ProfileCard() {
           />
         </div>
 
-        <div className="flex flex-col gap-[0.25rem]">
-          <h2 className="text-[1rem] font-semibold text-gray-900">
+        <div className="flex flex-col gap-[0.25rem] font-inter">
+          <h2 className="text-[1rem] font-semibold text-text-light  ">
             Alex Morgan
           </h2>
-          <div className="flex items-center gap-[0.5rem] text-[0.875rem] text-gray-500">
+          <div className="flex items-center gap-[0.5rem] text-[1rem] font-normal text-text-secondary ">
             <span>alexamorgan@gmail.com</span>
 
-          <FiMail className="w-[1.125rem] h-[1.125rem] text-gray-500" />
+          <FiMail className="w-[1rem] h-[1rem] text-text-secondary" />
           </div>
         </div>
       </div>
 
       <div className="mt-[1.5rem]">
-        <h3 className="text-[1.125rem] font-semibold text-gray-900 mb-[0.75rem]">
+        <h3 className="text-[1.125rem] font-semibold text-text-light mb-[0.75rem] font-open-sans ">
           Mission/Bio
         </h3>
-        <p className="text-[0.875rem] text-gray-600 leading-[1.375rem]">
+        <p className="text-[0.875rem] text-text-secondary font-normal  leading-[1.375rem] font-inter">
           Global Manufacturing Co. is a leading Tier 1 and Tier 2 supplier
           specializing in high-tolerance components, advanced material
           production, and efficient sub-assembly modules. With over 15 years of
@@ -42,10 +42,10 @@ export default function ProfileCard() {
       </div>
 
       <div className="mt-[1.5rem]">
-        <h3 className="text-[1.125rem] font-semibold text-gray-900 mb-[0.75rem]">
+        <h3 className="text-[1.125rem] font-semibold text-text-primary mb-[0.75rem] font-open-sans">
           Member Since
         </h3>
-        <p className="text-[0.875rem] text-gray-600">August 2023</p>
+        <p className="text-[0.875rem] text-text-setting-dark font-inter font-normal">August 2023</p>
       </div>
     </div>
   );
