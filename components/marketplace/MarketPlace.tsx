3607ab46207fe9eef00e02ba755b3bb8b0848c1f
Techforge25 <github@techforgeinnovations.com>
2026-08-07 16:12:33 +0500

    debugging tabs changes

--- components/marketplace/MarketPlace.tsx
+++ components/marketplace/MarketPlace.tsx
@@ -19,16 +19,21 @@ export default function MarketPlace({ dateRange }: Props) {
 
   const currentTab = searchParams.get("tab") || MarketPlacetabs[0]?.id;
 
-  const handleTabChange = (tabId: string) => {
-    startTransition(() => {
-      const params = new URLSearchParams(searchParams.toString());
-      params.set("tab", tabId);
+const handleTabChange = (tabId: string) => {
+  console.log("Changing tab:", tabId);
 
-      router.replace(`?${params.toString()}`, {
-        scroll: false,
-      });
+  startTransition(() => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    params.set("tab", tabId);
+
+    console.log("New URL:", params.toString());
+
+    router.replace(`?${params.toString()}`, {
+      scroll: false,
     });
-  };
+  });
+};
 
   return (
     <>
