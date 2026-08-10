b4e1a8f884632d78551fb512b44d4c0785dc8360
Techforge25 <github@techforgeinnovations.com>
2026-08-10 09:39:23 +0500

    fixed sidebar

--- components/layouts/Sidenavbar/sidebarMenuItems.tsx
+++ components/layouts/Sidenavbar/sidebarMenuItems.tsx
@@ -1,12 +1,5 @@
 import Link from "next/link";
-import React from "react";
 import { sidebarMenuItems as menuItemsData } from "@/constants/sidebar/sidebar";
-
-interface NavigationItem {
-  name: string;
-  icon: React.JSX.Element;
-  path: string;
-}
 
 interface SidebarMenuItemsProps {
   pathname: string;
@@ -23,7 +16,7 @@ export default function SidebarMenuItems({ pathname }: SidebarMenuItemsProps) {
           <Link
             key={index}
             href={item.path}
-            className={`flex items-center whitespace-nowrap gap-3 px-3 py-3 rounded-lg transition-all duration-200 font-medium ${
+            className={`flex items-center font-inter font-normal whitespace-nowrap gap-3 px-3 py-3 rounded-lg transition-all duration-200 font-medium ${
               isSelected
                 ? "bg-brand-primary text-text-inverse"
                 : "text-black hover:bg-brand-primary hover:text-text-inverse"
