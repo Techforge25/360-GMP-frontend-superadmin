d8b5e99bfd3c3b80762ecca320bfb94227167e17
Techforge25 <github@techforgeinnovations.com>
2026-08-13 16:43:20 +0500

    fixed business profile buttons show

--- components/account-management/business-management/business-view-profile/AccountManagementBusiness.tsx
+++ components/account-management/business-management/business-view-profile/AccountManagementBusiness.tsx
@@ -1,25 +1,25 @@
-'use client'
-import BackButtonMain from '../../../common/BackButtonMain'
-import BusinessProfileManagement from './BusinessProfileManagement'
-import BusinessOperations from './BusinessOperations'
-import BusinessIntelligence from './BusinessIntelligence'
-import OperationalAndTradeProfile from './OperationalAndTradeProfile'
-import AmlAndTransactionProfile from './AmlAndTransactionProfile'
-import RequiredDocuments from './RequiredDocuments'
-import ActionButtons from '../../ActionButtons'
-import { useParams } from 'next/navigation'
-import { useQuery } from '@tanstack/react-query'
-import { keys } from '@/keys'
-import { viewBusinessProfiles } from '@/services/account-management'
+"use client";
+import BackButtonMain from "../../../common/BackButtonMain";
+import BusinessProfileManagement from "./BusinessProfileManagement";
+import BusinessOperations from "./BusinessOperations";
+import BusinessIntelligence from "./BusinessIntelligence";
+import OperationalAndTradeProfile from "./OperationalAndTradeProfile";
+import AmlAndTransactionProfile from "./AmlAndTransactionProfile";
+import RequiredDocuments from "./RequiredDocuments";
+import ActionButtons from "../../ActionButtons";
+import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { keys } from "@/keys";
+import { viewBusinessProfiles } from "@/services/account-management";
 
 export default function AccountManagementBusiness() {
-  const { id } = useParams()
+  const { id } = useParams();
   const { data, isPending } = useQuery({
     queryKey: [keys.viewBusinessProfile, id],
     queryFn: () => viewBusinessProfiles(id),
   });
 
-  const viewBusinessProfileData = data?.data
+  const viewBusinessProfileData = data?.data;
 
   return (
     <div>
@@ -32,12 +32,16 @@ export default function AccountManagementBusiness() {
         foundedDate={viewBusinessProfileData?.foundedDate}
         headOfficeCountry={viewBusinessProfileData?.headOffice?.country}
         ownerName={viewBusinessProfileData?.ownerName}
-        tradeName={viewBusinessProfileData?.tradeName ?? 'N/A'}
-        businessRegistrationNumber={viewBusinessProfileData?.businessRegistrationNumber}
-        taxIdentificationNumber={viewBusinessProfileData?.taxIdentificationNumber}
-        dunsNumber={viewBusinessProfileData?.dunsNumber ?? 'N/A'}
+        tradeName={viewBusinessProfileData?.tradeName ?? "N/A"}
+        businessRegistrationNumber={
+          viewBusinessProfileData?.businessRegistrationNumber
+        }
+        taxIdentificationNumber={
+          viewBusinessProfileData?.taxIdentificationNumber
+        }
+        dunsNumber={viewBusinessProfileData?.dunsNumber ?? "N/A"}
         operationHour={viewBusinessProfileData?.operationHour}
-        website={viewBusinessProfileData?.website ?? 'N/A'}
+        website={viewBusinessProfileData?.website ?? "N/A"}
         description={viewBusinessProfileData?.description}
         logo={viewBusinessProfileData?.logo}
       />
@@ -45,21 +49,68 @@ export default function AccountManagementBusiness() {
         addressLine={viewBusinessProfileData?.headOffice?.addressLine}
         city={viewBusinessProfileData?.headOffice?.city}
         country={viewBusinessProfileData?.headOffice?.country}
-        warehouseAddress={viewBusinessProfileData?.warehouseAddress ?? 'N/A'}
-        additionalWarehouseAddress={viewBusinessProfileData?.additionalWarehouseAddress ?? 'N/A'}
-        internationalOffices={viewBusinessProfileData?.internationalOffices ?? 'N/A'}
-        incoterms={viewBusinessProfileData?.incoterms ?? 'N/A'}
-        termsAndCapability={viewBusinessProfileData?.termsAndCapability ?? 'N/A'}
+        warehouseAddress={viewBusinessProfileData?.warehouseAddress ?? "N/A"}
+        additionalWarehouseAddress={
+          viewBusinessProfileData?.additionalWarehouseAddress ?? "N/A"
+        }
+        internationalOffices={
+          viewBusinessProfileData?.internationalOffices ?? "N/A"
+        }
+        incoterms={viewBusinessProfileData?.incoterms ?? "N/A"}
+        termsAndCapability={
+          viewBusinessProfileData?.termsAndCapability ?? "N/A"
+        }
       />
-      <BusinessIntelligence primaryContactPerson={viewBusinessProfileData?.primaryContactPerson}
+      <BusinessIntelligence
+        primaryContactPerson={viewBusinessProfileData?.primaryContactPerson}
         executiveAndLeadership={viewBusinessProfileData?.executiveAndLeadership}
         ownedByAnotherCompany={viewBusinessProfileData?.ownedByAnotherCompany}
-        parentCompany={viewBusinessProfileData?.parentCompany ?? 'N/A'}
+        parentCompany={viewBusinessProfileData?.parentCompany ?? "N/A"}
       />
-      <OperationalAndTradeProfile auditingAgency={viewBusinessProfileData?.operationalAndTradeProfile?.auditingAgency ?? "N/A"} tradeAffiliations={viewBusinessProfileData?.operationalAndTradeProfile?.tradeAffiliations ?? "N/A"} regionOfOperations={viewBusinessProfileData?.operationalAndTradeProfile?.regionOfOperations} />
-      <AmlAndTransactionProfile purpose={viewBusinessProfileData?.amlAndTransactionProfile?.purpose} revenueRange={viewBusinessProfileData?.amlAndTransactionProfile?.revenueRange ?? 'N/A'} tradeCorridors={viewBusinessProfileData?.amlAndTransactionProfile?.tradeCorridors} mainCounterParties={viewBusinessProfileData?.amlAndTransactionProfile?.mainCounterParties ?? "N/A"} pep={viewBusinessProfileData?.amlAndTransactionProfile?.pep} evidenceOfFunds={viewBusinessProfileData?.evidenceOfFunds} />
-      <RequiredDocuments certificateOfIncorporation={viewBusinessProfileData?.certificateOfIncorporation} taxRegistrationCertificate={viewBusinessProfileData?.taxRegistrationCertificate} shareHolderRegister={viewBusinessProfileData?.shareHolderRegister} operatingLicense={viewBusinessProfileData?.operatingLicense} />
-      <ActionButtons id={viewBusinessProfileData?._id} />
+      <OperationalAndTradeProfile
+        auditingAgency={
+          viewBusinessProfileData?.operationalAndTradeProfile?.auditingAgency ??
+          "N/A"
+        }
+        tradeAffiliations={
+          viewBusinessProfileData?.operationalAndTradeProfile
+            ?.tradeAffiliations ?? "N/A"
+        }
+        regionOfOperations={
+          viewBusinessProfileData?.operationalAndTradeProfile
+            ?.regionOfOperations
+        }
+      />
+      <AmlAndTransactionProfile
+        purpose={viewBusinessProfileData?.amlAndTransactionProfile?.purpose}
+        revenueRange={
+          viewBusinessProfileData?.amlAndTransactionProfile?.revenueRange ??
+          "N/A"
+        }
+        tradeCorridors={
+          viewBusinessProfileData?.amlAndTransactionProfile?.tradeCorridors
+        }
+        mainCounterParties={
+          viewBusinessProfileData?.amlAndTransactionProfile
+            ?.mainCounterParties ?? "N/A"
+        }
+        pep={viewBusinessProfileData?.amlAndTransactionProfile?.pep}
+        evidenceOfFunds={viewBusinessProfileData?.evidenceOfFunds}
+      />
+      <RequiredDocuments
+        certificateOfIncorporation={
+          viewBusinessProfileData?.certificateOfIncorporation
+        }
+        taxRegistrationCertificate={
+          viewBusinessProfileData?.taxRegistrationCertificate
+        }
+        shareHolderRegister={viewBusinessProfileData?.shareHolderRegister}
+        operatingLicense={viewBusinessProfileData?.operatingLicense}
+      />
+      {viewBusinessProfileData?.status !== "rejected" &&
+        viewBusinessProfileData?.status !== "approved" && (
+          <ActionButtons id={viewBusinessProfileData?._id} />
+        )}
     </div>
-  )
+  );
 }
