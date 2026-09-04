83b995059ce3227617d0fc8037fb77e4fd74f394
Techforge25 <github@techforgeinnovations.com>
2026-09-04 11:29:29 +0500

    fixed account management tabs name

--- constants/acount-management/AccountManagementtabs.tsx
+++ constants/acount-management/AccountManagementtabs.tsx
@@ -2,13 +2,13 @@ import { FiBriefcase, FiUsers } from "react-icons/fi";
 export const accountManagementTabs = [
   {
     id: "all-user",
-    label: "All User",
+    label: "All Users",
     icon: <FiUsers  className="w-[1.1rem] h-[1.1rem]"/>,
     active: true,
   },
   {
     id: "all-business",
-    label: "All Business",
+    label: "All Businesses",
     icon: <FiBriefcase className="w-[1.1rem] h-[1.1rem]" />,
   },
 ];
